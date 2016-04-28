c53b8cce3ac49b66e6755592ad32ec21882dc5a4
knguyen <nguyentvk@hdviet.com>
2016-04-28 11:43:16 +0700

    a

--- common.h
+++ common.h
@@ -8,8 +8,8 @@
 #define PWM_Effect_Max					1530
 
 //dung de khoi dong/tat quadrotor
-#define PWM_ON_OFF_MIN					1080
-#define PWM_ON_OFF_MAX					1120
+#define PWM_ON_OFF_MIN					1075
+#define PWM_ON_OFF_MAX					1125
 
 //----------------------//
 //				 ORANGE							//
@@ -32,10 +32,12 @@
 #define LED_D_14_LOW 						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET)
 #define LED_D_15_LOW 						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET)
 
-#define ABS(x)         					(x < 0) ? (-x) : x
+#define ABS(x)         					((x < 0) ? (-x) : x)
 #define M_PI 										(float)3.1415926535
 #define PI											(float)3.1415926535
 #define RAD_TO_DEG 							(float)(180/PI)
+#define MAX(a,b)								( (a > b) ? a : b )
+#define MIN(a,b)								( (a < b) ? a : b )
 
 
 
